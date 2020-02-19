c91ebcd6ef9f4101ab4a121a5afde93e8c9970f8
alokkg7cr <61189285+alokkg7cr@users.noreply.github.com>
2020-02-19 15:08:09 +0530

    commit 2

--- app.js
+++ app.js
@@ -1,41 +1,96 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//Initiallising node modules
+const express = require("express");
+const bodyParser = require("body-parser");
+const sql = require("mssql");
+const app = express();
+const cors          = require('cors');
+let login = false;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let proposedEndDate_Casted;
+let actualStartedOn_Casted;
+let actualEndedOn_Casted;
+let assignedOn_Casted;
 
-var app = express();
+let actualStartedOnEdit_Casted;
+let actualEndedOnEdit_Casted;
+let assignedOnEdit_Casted;
+let proposedEndDateEdit_Casted;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Setting Base directory
+app.use(bodyParser.json());
+app.use(cors());
+app.options('*', cors());
+app.use(bodyParser.json({limit: '10mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
+//CORS Middleware
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS, PUT"
+  );
+  next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+//Initiallising connection string
+var dbConfig = {
+    user: 'alok.kumar',
+        password: 'infy@6176',
+        server: 'spiderdevdbsrv.centralindia.cloudapp.azure.com',
+        database: 'G7CR-SPIDERS-V1-Dev',
+        port:25282
+};
+
+sql.connect(dbConfig, function (err) {
+  if (err) {
+    console.log("Error while connecting database :- " + err);
+    res.send(err);
+  }
+  else{
+    console.log('connected successfully')
+    login =true;
+  }
+
 });
 
-module.exports = app;
+
+
+//ALLOW PATHS WITHOUT TOKEN AUTHENTICATION
+
+
+/* CREATE TOKEN FOR USE */
+
+  app.get("/api/comment", function(req , res,next){
+
+			var request = new sql.Request();
+			// query to the database
+			request.query("select * from ReadOnly.HelloWorld", function (err, recordset) {
+				if (err) {
+					console.log("Error while querying database :- " + err);
+					res.send(err);
+				}
+				else {
+          const attnd=[];
+          let abc=recordset
+          //console.log(abc.recordset)
+          res.status(200).json({
+            message:'hello world fetched successfully!!',
+            attnd:abc.recordset
+          })
+				}
+			});
+	//	}
+  });
+
+  ///////////////////////////////////////////////////////////////////////////////
+//Setting up server
+var server = app.listen(process.env.PORT || 5000, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
